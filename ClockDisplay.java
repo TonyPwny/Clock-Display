fa71bff5bee78f4d4e32186e0e42cc520bb61a75
Anthony Tiongson <anthonytiongson@me.com>
2018-09-24 20:05:11 -0400

    12HourInt branch

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -1,9 +1,11 @@
 
 /**
  * The ClockDisplay class implements a digital clock display for a
- * European-style 24 hour clock. The clock shows hours and minutes. The 
- * range of the clock is 00:00 (midnight) to 23:59 (one minute before 
+ * 12 hour clock. The clock shows hours and minutes and an indicator for AM/PM. The 
+ * range of the clock is 12:00am (midnight) to 11:59pm (one minute before 
  * midnight).
+ * 
+ * The clock maintains the hour time internally as values from 1-12.
  * 
  * The clock display receives "ticks" (via the timeTick method) every minute
  * and reacts by incrementing the display. This is done in the usual clock
